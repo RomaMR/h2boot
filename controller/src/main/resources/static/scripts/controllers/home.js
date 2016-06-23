77bcbf2c4824f85ecfe302abd3014dc1ef066682
romamr <mudryiroman93@gmail.com>
2016-06-24 01:53:46 +0300

    home page

--- controller/src/main/resources/static/scripts/controllers/home.js
+++ controller/src/main/resources/static/scripts/controllers/home.js
@@ -3,5 +3,20 @@
 angular.module('h2boot')
     .controller('Home', function (Storage, toaster) {
         var vm = this;
-        
+
+        vm.post = {};
+
+        vm.save = function () {
+            Storage.save({
+                    model: 'typicode',
+                    id: vm.post.id
+                },
+                vm.post.id,
+                function () {
+                    toaster.success('Saved', vm.post.id);
+                },
+                function (e) {
+                    toaster.error('Error', e.data.message);
+                });
+        };
     });
